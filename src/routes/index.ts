5235f2bb3c464eb17860e22afc527b7312d2e85b
komann <niclas.komander@msg-gillardon.de>
2022-02-04 17:26:51 +0100

    added image to api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import images from './api/image';
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
   res.send('main api route');
   console.log('/api main api route was visited');
 });
+
+routes.use('/image', images);
 
 export default routes;
