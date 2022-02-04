b2faf497ae379873f0e335ea0134ec713d40cda1
komann <niclas.komander@msg-gillardon.de>
2022-02-04 12:21:17 +0100

    init prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express from "express";
-import routes from './routes/index'
-
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 const port = 3000;
 
-app.use('/api',routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
-
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 const routes = express.Router();
 
-routes.get('/',(req,res) => {
-    res.send("main api route");
-    console.log("/api main api route was visited")
+routes.get('/', (req, res) => {
+  res.send('main api route');
+  console.log('/api main api route was visited');
 });
-
 
 export default routes;
